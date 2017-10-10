e569f40598d2fe8036defa9453d4b3b197ca52c8
m a r i e 2 :) <mbremner@andrew.cmu.edu>
2017-10-09 22:48:51 -0400

    first commit

---
+++ embedded_script.sh
@@ -0,0 +1,79 @@
+
+# everything you need to make kernel / any user app
+user_setup()
+{
+    echo "setting up..."
+    unset CROSS_COMPILE
+    unset ARCH
+    cur_dir="$(pwd)"
+    echo "going into busybox-1.21.0..."
+    cd ~/busybox-1.21.0
+    echo "sourcing busybox-configure..."
+    source busybox-configure.bash
+    echo "making busybox..."
+    colormake
+    echo "exprting CROSS_COMPILE and ARCH env variables..."
+    export CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm
+    echo "installing headers"
+    cd ~/kernel
+    colormake headers_install
+    cd "$cur_dir"
+   echo "Done!"
+}
+
+# everything you need for modules to make
+kernel_setup()
+{
+    export CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm
+    cur_dir="$(pwd)"
+    adb reboot-bootloader
+    cd ~/kernel
+    RAMDISK=~/nakasi-jdq39/boot-img/boot.img-ramdisk-root.gz
+    fastboot boot arch/arm/boot/zImage $RAMDISK
+    cd "$cur_dir"
+}
+
+# pass in the module name as an argument
+# makes the module and pushes onto the tablet
+make_module()
+{
+    cur_dir="$(pwd)"
+    cd ~/kernel
+    colormake M=rtes/modules/$1
+    adb push rtes/modules/$1/$1.ko data
+    cd "$cur_dir"
+}
+
+make_kernel()
+{
+    cur_dir="$(pwd)"
+    export CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm
+    cd ~/kernel
+    colormake
+    cd "$cur_dir"
+}
+
+make_user()
+
+{
+    cur_dir="$(pwd)"
+    cd ~/kernel
+    make headers_install
+    cd ~/kernel/rtes/apps/$1
+    colormake
+    adb push $1 data
+    cd "$cur_dir"
+}
+
+new_kernel_repo()
+{
+    cd ~/kernel
+    adb pull /proc/config.gz
+    gunzip config.gz
+    mv config .config
+    export CROSS_COMPILE=arm-linux-gnueabi- ARCH=arm
+    make oldconfig
+    make
+    file arch/arm/boot/compressed/vmlinux
+}
+
