020641a370cdd8445fc48779c4e9f2e38313e4ae
*~ m a r i e ~* <mbremner@andrew.cmu.edu>
2017-11-05 11:36:40 -0500

    adding hotpluggit add embedded_script.shgit add embedded_script.shgit add embedded_script.sh

--- embedded_script.sh
+++ embedded_script.sh
@@ -44,6 +44,7 @@ kernel_setup()
     RAMDISK=~/nakasi-jdq39/boot-img/boot.img-ramdisk-root.gz
     fastboot boot arch/arm/boot/zImage $RAMDISK
     cd "$cur_dir"
+    adb shell "source /data/auto_hotplug.sh"
 }
 
 # pass in the module name as an argument
